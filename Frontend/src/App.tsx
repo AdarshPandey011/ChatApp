fd3a67ebd52390b53a0e7e9c4275dad9575e3ad2
Adarsh Pandey <iadarsh012@gmail.com>
2024-12-26 01:06:45 +0530

    First commit

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@/components/ui/button"
+
+import SignupFormDemo from "./components/signup-form-demo"
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+      <SignupFormDemo/>
+    </div>
+    </>
+  )
+}
+
+export default App
